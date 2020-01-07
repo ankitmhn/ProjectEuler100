6cc6c28abf8197164a1079a85afef9d8d7d961e8
Ankit <find.ankit@gmail.com>
2020-01-07 16:03:43 +0530

    Problem 1 complete

--- problem1.js
+++ problem1.js
@@ -1,7 +1,9 @@
-module.exports.multiplesOf3and5 = function (n){
+function multiplesOf3and5(n){
     let sum = 0;
-    for(let i = 3; i <= n; i++) {
+    for(let i = 3; i < n; i++) {
         if(i % 3 == 0 || i % 5 == 0) sum += i;
     }
     return sum;
 }
+
+module.exports = multiplesOf3and5;
--- problem1.test.js
+++ problem1.test.js
@@ -1,3 +1,25 @@
-var solution = require('./problem1');
+const multiplesOf3and5 = require('./problem1');
 
-console.log(solution.multiplesOf3and5(1000));
+//trivial case
+test("Sum of multiples of 3 and 5 less than 10", () => {
+    expect(multiplesOf3and5(10)).toBe(23);
+})
+
+//multiplesOf3and5(1000) should return 233168. 
+test("Sum of multiples of 3 and 5 less than 1000", () => {
+    expect(multiplesOf3and5(1000)).toBe(233168);
+})
+//multiplesOf3and5(49) should return 543.  
+test("Sum of multiples of 3 and 5 less than 49", () => {
+    expect(multiplesOf3and5(49)).toBe(543);
+})
+
+//multiplesOf3and5(19564) should return 89301183. 
+test("Sum of multiples of 3 and 5 less than 19564", () => {
+    expect(multiplesOf3and5(19564)).toBe(89301183);
+})
+
+//multiplesOf3and5(8456) should return 16687353. 
+test("Sum of multiples of 3 and 5 less than 49", () => {
+    expect(multiplesOf3and5(8456)).toBe(16687353);
+})
