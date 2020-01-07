97607905e3c59b162eca04fd6df9b1a897e189d6
Ankit <find.ankit@gmail.com>
2020-01-07 16:53:10 +0530

    Problem 2 setup

---
+++ problem2.test.js
@@ -0,0 +1,5 @@
+// fiboEvenSum(10) should return 44.
+// fiboEvenSum(18) should return 3382.
+// fiboEvenSum(23) should return 60696.
+// fiboEvenSum(43) should return 350704366.
+// Your function should return an even value.
